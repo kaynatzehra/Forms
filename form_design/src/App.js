04eaf741c2846bec559590bfc37432bd2bc9184b
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-04-07 16:27:36 +0530

    Added: Form design

---
+++ form_design/src/App.js
@@ -0,0 +1,13 @@
+//import React,  {useState} from 'react'
+import './App.css';
+import MultipleInputs from './component/forms/MultipleInputs';
+
+const App = () => {
+  return (
+    <div>
+   <MultipleInputs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ form_design/src/component/forms/MultipleInputs.js
@@ -0,0 +1,56 @@
+// import ReactDOM from "react-router-dom";
+//import ReactDOM from 'react-dom';
+import  {useState} from 'react'
+
+//import React from 'react'
+
+const MultipleInputs =  () =>  {
+
+
+    const [name, setName] = useState("");
+    const [Number, setNumber] = useState("");
+    const [Email, setEmail] = useState("");
+    const [counter, setCounter]= useState(1)
+    
+    const increment=()=>{
+      setCounter(counter +1);
+    };
+      
+    return (
+      <div>
+          <form >
+      <label>Enter your FullName: 
+        <input type="text"  aria-label="Firstname" placeholder='Enter your Name' value={name}  
+          onChange={(e) => setName(e.target.value)}
+        />   
+      </label><br />
+      <label>Enter your Number: 
+        <input
+          type="text" placeholder='Enter your Number'
+          value={Number}  
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </label><br />
+      <label>Enter your Email: 
+        <input
+          type="text" placeholder='Enter your Email'
+          value={Email}  
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label><br />
+      <label>Enter your Password:       
+        <input
+          type="text" placeholder='Enter your Password'
+          value={name}  
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label><br />
+    </form>
+    <div>  
+       <button onClick={increment}>Submit</button>Number of Entries: {counter} </div>
+      </div>
+    )
+  }
+
+
+  export default MultipleInputs;
